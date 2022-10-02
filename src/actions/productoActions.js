d1607c9de1a044409ac639a5324edef4040da51b
Adotel <adriandotel@gmail.com>
2022-10-02 20:53:25 +0200

    Ya funciona todo con legacy create store

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -5,15 +5,20 @@ import {
     AGREGAR_PRODUCTO_ERROR    
 } from '../types'
 
+import clienteAxios from '../config/clienteAxios'
+
 // Crear Nuevos Productos
 export function crearNuevoProductoAction(producto) {
-    return ( dispatch ) => {
+    return async dispatch => {
         // Intenta agregar el producto, ponemos loading true
         dispatch( agregarProducto() ) 
         try {
-            // Si lo consigue añade producto al state
+            // Si lo consigue añade producto al state, insertar en la API
+            await clienteAxios.post('/productos', producto)
+
             dispatch ( agregarProductoExito(producto) )
         } catch (error) {
+            console.log(error)
             // Si falla añade el error
             dispatch ( agregarProductoError(true) )
         }
