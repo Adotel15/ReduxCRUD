e504c763939836f9006ebe7875c8c5bf5dda2481
Adotel <adriandotel@gmail.com>
2022-10-02 19:52:23 +0200

    Añadido types

--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -1,4 +1,11 @@
 
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR    
+} from '../types'
+
+
 // Cada reducer tiene su propio State
 
 const initialState = {
